f45501349d013be042573e95ee47d6c6a27753a5
adieu <adieu@adieu.me>
2012-05-10 00:42:22 +0800

    Wrap the user drop down in header with a block.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-template-bootstrap",
-    version="0.2.2",
+    version="0.2.3",
     license='BSD',
     description="A django template based on twitter's bootstrap project.",
     author='Ivan Diao',
