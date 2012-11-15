c45404a82ca7116de87257130fc7d9d799573be3
adieu <adieu@adieu.me>
2012-11-15 20:33:28 +0800

    Fix the bug that span inside .block_content does not display properly.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-template-bootstrap",
-    version="0.2.9",
+    version="0.2.10",
     license='BSD',
     description="A django template based on twitter's bootstrap project.",
     author='Ivan Diao',
